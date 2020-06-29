2d03cec4d99b2fc237eb5c0ea027f1341d998c02
Ronnie Geraghty <ronniegerag@gmail.com>
2020-06-29 19:03:00 -0400

    using interfaces with classes

--- classes-interfaces-01/src/app.ts
+++ classes-interfaces-01/src/app.ts
@@ -1,18 +1,30 @@
-interface Person {
+/* You could just make a custom type but interfaces 
+are more strick and are more often used.
+*/
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+// implements means that this class has the things the interface requires
+// you can implement more than one interface by listing them with commas between
+// You can also have things that aren't specificed in the implemented interface
+class Person implements Greetable {
+  name: string;
+  age = 24;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: "Ronnie",
-  age: 24,
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Ronnie");
 
 user1.greet("Hi there I am");
+console.log(user1);
