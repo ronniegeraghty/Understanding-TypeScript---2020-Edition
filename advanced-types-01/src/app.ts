055f1273f8589fe1422fdbf1a655e572f25471cf
Ronnie Geraghty <ronniegerag@gmail.com>
2020-07-08 17:29:17 -0400

    Intersection Types

--- advanced-types-01/src/app.ts
+++ advanced-types-01/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+//Intersection types let us combine other types.
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee; // This is the intersection type
+
+const e1: ElevatedEmployee = {
+  name: "Ronnie",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; // Will only be numbers since that is the only type Combinable and Numeric have in common.
