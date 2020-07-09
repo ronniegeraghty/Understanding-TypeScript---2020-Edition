7b5b7abc57f1cd8ec8df298759863a98c7428b2e
Ronnie Geraghty <ronniegerag@gmail.com>
2020-07-09 17:30:41 -0400

    Type Guards p2

--- advanced-types-01/src/app.ts
+++ advanced-types-01/src/app.ts
@@ -47,3 +47,33 @@ function printEmployeeInformation(emp: UnknownEmployee) {
 
 printEmployeeInformation(e1);
 printEmployeeInformation({ name: "Sophie", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading carpo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
