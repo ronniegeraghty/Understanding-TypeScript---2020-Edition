b9dfed44b72b01537d62af666da3d4cf50c8fad0
Ronnie Geraghty <ronniegerag@gmail.com>
2020-07-09 17:24:28 -0400

    Type Guards p1

--- advanced-types-01/src/app.ts
+++ advanced-types-01/src/app.ts
@@ -21,3 +21,29 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // Will only be numbers since that is the only type Combinable and Numeric have in common.
+
+//Type Guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    //This line above is a type guard using typeof
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  //console.log('Privileges: ' + emp.privileges); // This line gives an error because the UnknowEmployee only has this property when its an Admin
+  //Can check if the property exists for the employee
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Sophie", startDate: new Date() });
