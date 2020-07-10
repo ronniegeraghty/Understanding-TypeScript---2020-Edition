13e229aa2b7e7b4bcc0b1297934c19a0deb98b81
Ronnie Geraghty <ronniegerag@gmail.com>
2020-07-10 16:52:01 -0400

    Discriminated Union

--- advanced-types-01/src/app.ts
+++ advanced-types-01/src/app.ts
@@ -78,3 +78,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+//Discriminated Union
+// has one similar property (ex: type) between all objects or interfaces that tells what type of object it is.
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving with speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
